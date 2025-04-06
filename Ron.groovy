9474957d6519cc3b52564a2f3a5d7ec1ae25006a
KacperLa <klaskak@gmail.com>
2025-04-06 12:53:21 -0400

    Added a script to create a single link

--- Ron.groovy
+++ Ron.groovy
@@ -1,1 +1,15 @@
-// code here
+
+// Basic Paramaters
+double length  = 75;
+double D  = 60;
+
+CSG Link = new Cylinder(D/2.0, D/2.0, length, 64).toCSG();
+Link = Link.union(new Sphere(D/2.0, 32, 32).toCSG().movez(length));
+
+Link = Link.difference(new Cube(D, D, D).toCSG().movez(length).movex(-D/2));
+
+
+Link.setColor(javafx.scene.paint.Color.BLUE);
+
+return Link;
+
